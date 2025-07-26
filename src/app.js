a85caaa7e9248e4a755459b2b71453ebe7358a1a
shyam kumar <sksingh90405@gmail.com>
2025-07-26 19:50:07 +0530

    auth and middleware

--- src/app.js
+++ src/app.js
@@ -3,18 +3,33 @@ const express = require("express")
 
 const app = express()
 
+// handle auth and middleware for all GET, POST, ... requests
+app.use('/admin', (req,res,next) => {
+    console.log('admin auth is getting checked !');
+    const token = 'abcdef'
+    const isAdminAuthorised = token === 'abcdef'
 
-app.use("/user",
-    (req,res,next) => {
-        console.log('this is first response!');
-        // res.send('route handler 1')
+    if(!isAdminAuthorised){
+        res.status(401).send('Unauthorised request')
+    }else{
         next()
-    },
-    (req,res ) => {
-        console.log('this is second response!');
-        res.send('route handler 2')
     }
-)
+})
+
+
+app.get('/user', (req,res) => {
+    res.send('user data sent !')
+})
+
+
+app.get('/admin/getAllData', (req,res) => {
+   res.send('All data sent!')
+})
+
+
+app.get('/admin/deleteUser', (req,res) => {
+   res.send('Deleted a user !')
+})
 
 
 app.listen(3000, () => {
